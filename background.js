83d8ff768741c1e43aa98d81b8a803ca3203969c
sawanoboly <sawanoboriyu@higanworks.com>
2019-11-11 16:47:06 +0900

    use PageAction

--- background.js
+++ background.js
@@ -1,30 +1,25 @@
 function colorIcon() {
-  chrome.browserAction.setIcon({
-    path : {
-      "19": "Shifter_Color_19.png",
-      "38": "Shifter_Color_38.png"
+  chrome.tabs.query(
+    {currentWindow: true, active : true},
+    function(tabs){
+      chrome.pageAction.setIcon({
+        path : {
+          "19": "Shifter_Color_19.png",
+          "38": "Shifter_Color_38.png"
+        },
+        tabId : tabs.shift().id
+      });
     }
-  });
+  )
 }
-
-function monoIcon() {
-  chrome.browserAction.setIcon({
-    path : {
-      "19": "Shifter_Mono_19.png",
-      "38": "Shifter_Mono_38.png"
-    }
-  });
-}
-
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.isShifter) {
       console.log('Shifter!')
       colorIcon()
-    } else {
-      monoIcon()
     }
-  sendResponse()
+
+    sendResponse()
   }
 )
