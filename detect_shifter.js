7d7f193a8068936590bcf7fa847d8781ac6cb522
sawanoboly <sawanoboriyu@higanworks.com>
2019-11-13 22:21:22 +0900

    support mozilla

--- detect_shifter.js
+++ detect_shifter.js
@@ -1,3 +1,10 @@
-chrome.runtime.sendMessage(
-  {isShifter: document.documentElement.outerHTML.includes("created by Shifter Generator")
-}, function(response) {})
+if (document.childNodes.length === 3) {
+  chrome.runtime.sendMessage(
+    {isShifter: document.lastChild.textContent.includes("created by Shifter Generator")
+  }, function(response) {})
+} else {
+  chrome.runtime.sendMessage(
+    {isShifter: document.documentElement.outerHTML.includes("created by Shifter Generator")
+  }, function(response) {})
+}
+
